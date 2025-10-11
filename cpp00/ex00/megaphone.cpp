70620747f40c6935d973379f388d8682949f9fba
Buss_Matheus <matheus.buss.0000@gmail.com>
2025-10-11 02:03:19 +0100

    Made ex00 from cpp00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int	main(int argc, char** argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1;i < argc; ++i) {
+		std::string arg = argv[i];
+		for (std::size_t j = 0; j < arg.length(); ++j) {
+			std::cout << static_cast<char>(std::toupper(arg[j]));
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
