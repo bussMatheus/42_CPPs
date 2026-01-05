8cfb79ed6f5613f841fa7908624a6d72d9f830a6
Matheus Buss <135855248+bussMatheus@users.noreply.github.com>
2026-01-05 21:32:44 +0000

    handleAdd now checks for invalid field input

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,47 +1,5 @@
 #include "./PhoneBook.hpp"
-
-void	handleAdd(PhoneBook &pb, Contact &c)
-{
-    std::string f, l, n, p, s;
-
-	std::cout << "First Name: ";
-	if (!std::getline(std::cin, f))
-		return ;
-	std::cout << "Last Name: ";
-	if (!std::getline(std::cin, l))
-		return ;
-	std::cout << "Nickname: ";
-	if (!std::getline(std::cin, n))
-		return ;
-	std::cout << "Phone Number: ";
-	if (!std::getline(std::cin, p))
-		return ;
-	std::cout << "Darkest Secret: ";
-	if (!std::getline(std::cin, s))
-		return ;
-	c.setContact(f, l, n, p, s);
-	pb.addContact(c);
-	return;
-}
-
-void	handleSearch(PhoneBook &pb)
-{
-	int			index;
-	std::string input;
-
-	index = 0;
-	pb.searchContact();
-	std::cout << "Enter Index: ";
-	if(!std::getline(std::cin, input))
-		return ;
-	if (!input.empty() && std::isdigit(input[0]))
-	{
-		index = atoi(input.c_str());
-		pb.displayContacts(index);
-	}
-	if (!std::isdigit(input[0]))
-		std::cout << "Invalid. Try a digit between 0 and 9." << std::endl;
-}
+#include "./utils.hpp"
 
 int	main(void)
 {
