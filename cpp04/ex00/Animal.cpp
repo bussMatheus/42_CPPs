62b419f180d0fe0a026d971dbc1887fbfee34358
Buss_Matheus <matheus.buss.0000@gmail.com>
2026-01-13 01:19:14 +0000

    Makefiles corrected

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,35 @@
+#include "./Animal.hpp"
+
+Animal::Animal() : type("Animal")
+{
+	std::cout << "Animal constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal &other)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+	*this = other;
+}
+
+Animal	&Animal::operator=(const Animal &other)
+{
+	std::cout << "Animal assigment operator called" << std::endl;
+	if (this != &other)
+		this->type = other.type;
+	return (*this);
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+std::string	Animal::getType() const
+{
+	return (this->type);
+}
+
+void	Animal::makeSound() const
+{
+	std::cout << "* Animal makes some generic sound *" << std::endl;
+}
